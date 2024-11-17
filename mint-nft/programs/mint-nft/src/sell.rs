a52e6aa08a073e23965bdda4840b6571c1546827
chuasonglin1995 <chuasonglin1995@gmail.com>
2024-11-17 23:48:28 +0800

    add sell smart contract

--- mint-nft/programs/mint-nft/src/sell.rs
+++ mint-nft/programs/mint-nft/src/sell.rs
@@ -1,24 +1,69 @@
 use {
 	anchor_lang::{
-		prelude::*,
+		prelude::*, solana_program::system_program,
 	},
 	anchor_spl::{
-		associated_token::AssociatedToken,
+		associated_token::{self, AssociatedToken},
 		token::{self, Token},
-	},
-}
-
-
+	}
+};
 
 
 pub fn sell(
-	ctx: Context<SellNft>,r
+	ctx: Context<SellNft>,
 	sale_lamports: u64
 ) -> Result<()> {
 	
+	msg!("Initiating transfer of {} lamports", sale_lamports);
+	msg!("Purchaser (sending lamports): {}", &ctx.accounts.buyer_authority.key());
+	msg!("Seller (receiving lamports): {}", &ctx.accounts.owner_authority.key());
+	token::transfer(
+		CpiContext::new(
+			ctx.accounts.system_program.to_account_info(),
+			token::Transfer {
+				from: ctx.accounts.buyer_authority.to_account_info(),
+				to: ctx.accounts.owner_authority.to_account_info(),
+				authority: ctx.accounts.buyer_authority.to_account_info(),
+			}
+		),
+		sale_lamports
+	)?;
 
+	msg!("Lamports transferred successfully");
 
+	msg!("Creating buyer token account");
+	msg!("Buyer Token Address: {}", &ctx.accounts.buyer_token_account.key());
+	associated_token::create(
+		CpiContext::new(
+			ctx.accounts.associated_token_program.to_account_info(),
+			associated_token::Create {
+				payer: ctx.accounts.buyer_authority.to_account_info(),
+				associated_token: ctx.accounts.buyer_token_account.to_account_info(),
+				authority: ctx.accounts.buyer_authority.to_account_info(),
+				mint: ctx.accounts.mint.to_account_info(),
+				system_program: ctx.accounts.system_program.to_account_info(),
+				token_program: ctx.accounts.token_program.to_account_info(),
+			},
+		)
+	)?;
 
+	msg!("Transferring NFT ownership");
+	token::transfer(
+		CpiContext::new(
+			ctx.accounts.token_program.to_account_info(),
+			token::Transfer {
+				from: ctx.accounts.owner_token_account.to_account_info(),
+				to: ctx.accounts.buyer_token_account.to_account_info(),
+				authority: ctx.accounts.owner_authority.to_account_info(),
+			}
+		),
+		1
+	)?;
+
+	msg!("NFT transferred successfully");
+	msg!("Sale completed successfully");
+
+	Ok(())
 }
 
 #[derive(Accounts)]
